113f48f7118bdf98d89cfcf0a9fe06b4e6c71404
AnthonyQuispe <anthony.s.quispe@gmail.com>
2025-05-27 22:12:18 -0400

    Finish styling and adding menumodal functionality.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,46 +1,108 @@
 import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import "./Nav.scss";
 import Logo from "../../assets/logo/ScribbleMeMellowLogo.svg";
 import MenuIcon from "../../assets/Icons/MenuIcon.svg";
 import ProfileIcon from "../../assets/Icons/ProfileIcon.svg";
 import CartIcon from "../../assets/Icons/CartIcon.svg";
-import { Link } from "react-router-dom";
+import CloseIcon from "../../assets/Icons/CloseIcon.png";
 
-export default function Nav() {
+export default function Nav({ MenuModal }) {
+  const [modal, setModal] = useState(false);
+
+  const getNavLinkClass = ({ isActive }) =>
+    `nav__list-items-link ${isActive ? "nav__list-items-link--active" : ""}`;
+
+  function ModalHandler() {
+    setModal(!modal);
+  }
+
+  function MenuModal({ closeModal }) {
+    const getMenuLinkClass = ({ isActive }) =>
+      `menu-modal-list-items-link ${
+        isActive ? "menu-modal-list-items-link--active" : ""
+      }`;
+
+    return (
+      <div className="menu-modal">
+        <ul className="menu-modal-list">
+          <button
+            className="menu-modal-list-items menu-modal-list-items--close"
+            onClick={closeModal}
+          >
+            <img src={CloseIcon} className=" menu-modal-list-close-img" />
+          </button>
+          <li className="menu-modal-list-items ">
+            <Link to="/" className=" menu-modal-list-logo-container">
+              <img
+                src={Logo}
+                className=" menu-modal-list-logo"
+                alt="Scribble Me Mellow Logo"
+              />
+            </Link>
+          </li>
+          <li className="menu-modal-list-items ">
+            <NavLink to="/shop" className={getMenuLinkClass}>
+              Shop
+            </NavLink>
+          </li>
+          <li className="menu-modal-list-items ">
+            <NavLink to="/" className={getMenuLinkClass}>
+              Home
+            </NavLink>
+          </li>
+          <li className="menu-modal-list-items ">
+            <NavLink to="/about" className={getMenuLinkClass}>
+              About
+            </NavLink>
+          </li>
+          <li className="menu-modal-list-items ">
+            <NavLink to="/contact" className={getMenuLinkClass}>
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <nav className="nav">
+      {modal && <MenuModal closeModal={() => setModal(false)} />}
       <ul className="nav__list nav__list--desktop">
         <li className="nav__list-items">
-          <Link className="nav__list-items-link">Shop</Link>
+          <NavLink to="/shop" className={getNavLinkClass}>
+            Shop
+          </NavLink>
         </li>
         <li className="nav__list-items">
-          <Link to="/home" className="nav__list-items-link">
+          <NavLink to="/" className={getNavLinkClass}>
             Home
-          </Link>
+          </NavLink>
         </li>
         <li className="nav__list-items ">
           <Link
-            to="/home"
+            to="/"
             className="nav__list-items-link nav__list-items-link--logo"
           >
             <img src={Logo} />
           </Link>
         </li>
         <li className="nav__list-items">
-          <Link to="/about" className="nav__list-items-link">
+          <NavLink to="/about" className={getNavLinkClass}>
             About
-          </Link>
+          </NavLink>
         </li>
         <li className="nav__list-items">
-          <Link to="/contact" className="nav__list-items-link">
+          <NavLink to="/contact" className={getNavLinkClass}>
             Contact
-          </Link>
+          </NavLink>
         </li>
       </ul>
 
       <ul className="nav__list nav__list--mobile">
         <li className="nav__list-items">
-          <button className="nav__list-items-button">
+          <button className="nav__list-items-button" onClick={ModalHandler}>
             <img
               src={MenuIcon}
               alt="Menu Button"
